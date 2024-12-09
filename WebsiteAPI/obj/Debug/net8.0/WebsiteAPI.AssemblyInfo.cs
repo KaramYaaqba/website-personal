e12c8731ef1652b90c34b830b21a0cec5f676479
Karam Yaaqba <kyaaqba@hotmail.com>
2024-12-09 09:08:50 +0200

    change permission

--- WebsiteAPI/obj/Debug/net8.0/WebsiteAPI.AssemblyInfo.cs
+++ WebsiteAPI/obj/Debug/net8.0/WebsiteAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556b0f4dd4d2a011b073c76ec6822984e610775e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54418ee5f7319dd9d1d3714987faef0c04b6c12b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
