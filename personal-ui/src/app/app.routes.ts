b2c4b59cef0c3f24ce5a369e626f4dc48f6ac827
Karam Yaaqba <kyaaqba@hotmail.com>
2024-12-18 14:51:33 +0200

    fox navigation

--- personal-ui/src/app/app.routes.ts
+++ personal-ui/src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { ProjectsComponent } from './features/projects/projects.component';
 import { ContactComponent } from './features/contact/contact.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'about', pathMatch: 'full' },
     { path: 'about', component: AboutComponent },
     { path: 'projects', component: ProjectsComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: '', redirectTo: '/about', pathMatch: 'full' }, // Default route
+    { path: '**', redirectTo: '/about' } // Wildcard for unmatched routes
   ];
   
