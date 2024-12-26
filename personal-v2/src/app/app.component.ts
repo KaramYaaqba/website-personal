e084f572b7f7008e27e30336c71850d53e02435e
Karam Yaaqba <kyaaqba@hotmail.com>
2024-12-26 15:43:55 +0200

    add new design

---
+++ personal-v2/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [animate('0.5s ease-in')]),
+    ]),
+  ],
+})
+export class AppComponent {
+  title = 'personal-v2';
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
