66a628bc6f9aab263e12e7403d8521473c5acb24
Karam Yaaqba <kyaaqba@hotmail.com>
2024-12-28 11:31:24 +0200

    update about

--- personal-v2/src/app/app.component.ts
+++ personal-v2/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
